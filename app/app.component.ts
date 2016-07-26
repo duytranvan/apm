3cfe2744ef75b044ab509e88308eb6680915f3bf
Duy TRAN VAN <duytranvan@gmail.com>
2016-07-26 17:27:08 +0200

    dev en cours

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,14 @@
 import {Component} from 'angular2/core';
 import {ProductListComponent} from './products/product-list.component';
+import {ProductService} from './products/product.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx' // load all features
 
 @Component ({
     selector: 'pm-app',
     template: '  <div> <h1>{{pageTitle}}</h1><pm-products></pm-products></div>',
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService, HTTP_PROVIDERS]
     
 })
 export class AppComponent {
